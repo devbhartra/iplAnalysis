af887acb905a8324006ed37733ab172d0f004096
AprameyaKulkarni <37021026+AprameyaKulkarni@users.noreply.github.com>
2019-10-29 13:25:37 +0530

    Update task2.py

--- Assignment3/task2.py
+++ Assignment3/task2.py
@@ -6,6 +6,10 @@ import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+def sortrecord(rdd):
+    l = rdd.sortBy(lambda x:x[1],ascending=False).collect()[:3]
+    l = list(map(lambda x:x[0],l))
+    print(*l,sep=',')
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -14,13 +18,15 @@ if __name__ == "__main__":
     sc = SparkContext(appName="ThreeMostCommonHashtags")
     ssc = StreamingContext(sc, int(sys.argv[2]))
 
-    lines = ssc.socketTextStream('localhost', 9009)#.window(sys.argv[1])
+    lines = ssc.socketTextStream('localhost', 9009)
+    lines=lines.window(int(sys.argv[1]),slideDuration=None)
     counts = lines.map(lambda line: line.split(';')[7])\
+                  .flatMap(lambda x:x.split(','))\
                   .map(lambda word: (word, 1))\
-                  .reduceByKey(lambda a, b: a+b)\
-                  #.collect() transformedDstream has no attribute collect
-                  #.sortByValues()\   has no attribute sortbyvalues!
-    counts.pprint(3)
+                  .reduceByKey(lambda a, b: a+b)
+    counts.foreachRDD(sortrecord)
+    #counts.pprint(3)
 
     ssc.start()
     ssc.awaitTermination()
+    ssc.stop()
