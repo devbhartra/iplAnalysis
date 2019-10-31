0e994baa80ea8dde7f433e5515219bfc2fa6cc85
AprameyaKulkarni <37021026+AprameyaKulkarni@users.noreply.github.com>
2019-10-31 22:29:11 +0530

    Update task2.py

--- Assignment3/task2.py
+++ Assignment3/task2.py
@@ -1,13 +1,18 @@
 
 from __future__ import print_function
 
+import findspark
+findspark.init()
 import sys
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+
 def sortrecord(rdd):
-    l = rdd.sortBy(lambda x:x[1],ascending=False).collect()[:3]
+    l = rdd.sortBy(lambda x:x[1],ascending=False).collect()[:5]
+    l.sort(key=lambda x:x[0],reverse=False)
+    l.sort(key=lambda x:x[1],reverse=True)
     l = list(map(lambda x:x[0],l))
     print(*l,sep=',')
 
@@ -23,10 +28,10 @@ if __name__ == "__main__":
     counts = lines.map(lambda line: line.split(';')[7])\
                   .flatMap(lambda x:x.split(','))\
                   .map(lambda word: (word, 1))\
-                  .reduceByKeyAndWindow(lambda a, b: a+b,None,int(sys.argv[1]),int(sys.argv[2]))
+                  .reduceByKeyAndWindow(lambda a, b: a+b,None,int(sys.argv[1]),slideDuration=1)
     counts.foreachRDD(sortrecord)
-    #counts.pprint(3)
+    #counts.pprint()
 
     ssc.start()
-    ssc.awaitTermination()
+    ssc.awaitTermination(25)
     ssc.stop()
