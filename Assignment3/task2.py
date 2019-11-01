3903bd8fa41b524b068a42cc523c6cc21113de39
Dev Bhartra <devbhartra183@gmail.com>
2019-11-01 20:42:44 +0530

    For the love of god, work

--- Assignment3/task2.py
+++ Assignment3/task2.py
@@ -3,41 +3,47 @@ from __future__ import print_function
 import findspark
 findspark.init()
 import sys
-
-from pyspark import SparkContext
+from operator import add
 from pyspark.streaming import StreamingContext
+from pyspark import SparkContext, SparkConf
 
-def remove_empty_string_if_any(l):
-    for i in l:
-        if i[0]=='':
-            l.remove(i)
-            return l
-    return l[:-1]
+
+
+def splitLines(linesOfRecords):
+    hashtags = linesOfRecords.split(";")[7]
+    if(',' in hashtags):
+        return hashtags.split(",")
+    return [hashtags]
+
 
 def sortrecord(rdd):
-    l = rdd.sortBy(lambda x:x[1],ascending=False).collect()[:6]
-    l = remove_empty_string_if_any(l)
-    l.sort(key=lambda x:x[0],reverse=False)
-    l.sort(key=lambda x:x[1],reverse=True)
-    l = list(map(lambda x:x[0],l))
-    print(*l,sep=',')
+    rddIntermediate = rdd.sortBy(lambda a: (-a[1],a[0])).filter(lambda x : x[0] != '') #descending sort
+    sortedIterable = rddIntermediate.collect()
+    if(sortedIterable != []):
+        print(sortedIterable[0][0],sortedIterable[1][0],sortedIterable[2][0],sortedIterable[3][0],sortedIterable[4][0],sep=",")
 
 if __name__ == "__main__":
+
     if len(sys.argv) != 3:
         print("Usage: task2.py <Windows size> <batch duration>", file=sys.stderr)
         sys.exit(-1)
-    sc = SparkContext(appName="ThreeMostCommonHashtags")
-    ssc = StreamingContext(sc, int(sys.argv[2]))
+
+    windowSize = int(sys.argv[1])
+    batch = int(sys.argv[2])
+
+    configuration = SparkConf()
+    configuration.setAppName("ThreeMostCommonHashtags")
+    sc=SparkContext(conf=configuration)
+
+    #sc = SparkContext(appName="ThreeMostCommonHashtags")
+    ssc = StreamingContext(sc, batch)
 
     lines = ssc.socketTextStream('localhost', 9009)
-    #lines=lines.window(int(sys.argv[1]),slideDuration=None)
-    counts = lines.map(lambda line: line.split(';')[7])\
-                  .flatMap(lambda x:x.split(','))\
-                  .map(lambda word: (word, 1))\
-                  .reduceByKeyAndWindow(lambda a, b: a+b,None,int(sys.argv[1]),slideDuration=1)
+
+    counts = lines.window(windowSize,1).flatMap(splitLines).map(lambda x : (x,1)).reduceByKey(add)
+
     counts.foreachRDD(sortrecord)
     #counts.pprint()
-
     ssc.start()
     ssc.awaitTermination(25)
     ssc.stop()
